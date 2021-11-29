569f90fd859ab8a0168b11eede01ff2f4e4495f0
Coder J <buildingguy@sbcglobal.net>
2021-11-28 20:57:26 -0800

    place prompts in text area

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,39 @@
 // Assignment code here
 //https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
 //https://stackoverflow.com/questions/57984670/how-do-i-display-a-list-when-button-is-clicked-in-javascript
-function promptDisplay () {
-   var prompts = ["Password is at least 8 characters.", 
+
+// Method 1 Reference //
+
+// function promptsDisplay () {
+//    var prompts = ["Password is at least 8 characters.", 
+//   "Password has a mixture of both uppercase and lowercase letters", 
+//   "Password has a mixture of letters and numbers", 
+//   "Password includes at least one special character, e.g., ! @ # ? ]"]
+
+//   for(var i = 0; i < prompts.length; i++) {
+//     console.log(prompts[i]);
+//   }
+// }
+
+// document.getElementById("generate").addEventListener("click", function () {
+//   document.getElementById("password").innerHTML= "testing";
+// });
+
+// Method //
+
+function showPrompts() {
+  var promptList = new Array("Password is at least 8 characters.", 
   "Password has a mixture of both uppercase and lowercase letters", 
   "Password has a mixture of letters and numbers", 
-  "Password includes at least one special character, e.g., ! @ # ? ]"]
+  "Password includes at least one special character, e.g., ! @ # ? ]");
+  promptList.sort();
 
-  for(var i = 0; i < prompts.length; i++) {
-    console.log(prompts[i]);
-  }
+  document.getElementById("password").innerHTML = promptList.join(" ");
 }
 
-document.getElementById("generate").addEventListener("click", function() {
-  document.getElementById("password").innerHTML = "Testing";
-});
+showPrompts();
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
